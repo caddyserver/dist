a6652785ffb1124ad27b7f2d07aef1368a690208
Mohammed Al Sahaf <msaa1990@gmail.com>
2020-07-14 20:49:18 +0300

    packer: use the apt repo instead of downloading the binary from Github releases, which simplifies the Packer build template and enables auto-update of Caddy

--- packer/README.md
+++ packer/README.md
@@ -8,12 +8,11 @@ First make sure you have the latest tags from the `caddyserver/caddy` repo (`git
 Then you can get the Caddy version with a command, and run the script:
 
 ```bash
-export CADDY_VERSION=$(git -C ../../caddy describe --abbrev=0 --tags HEAD)
-DIGITALOCEAN_TOKEN=foobar packer build -var caddy_version=$CADDY_VERSION do-marketplace-image.json
+DIGITALOCEAN_TOKEN=foobar packer build do-marketplace-image.json
 ```
 
 Be sure to replace:
-- The `-C` flag with the actual path to your copy of the caddy repository (or just set the caddy_version variable manually, like `-var 'caddy_version=v2.0.0-beta9'`)
+
 - Your DigitalOcean API key
 
 The scripts `90-cleanup.sh` and `99-img_check.sh` are from [DO's own repo](https://github.com/digitalocean/marketplace-partners/tree/master/scripts).
--- packer/scripts/20-caddy.sh
+++ packer/scripts/20-caddy.sh
@@ -1,31 +1,9 @@
 #!/bin/bash
 set -eux;
 
-groupadd --system caddy
-useradd --system \
-    --gid caddy \
-    --create-home \
-    --home-dir /var/lib/caddy \
-    --shell /usr/sbin/nologin \
-    --comment "Caddy web server" \
-    caddy
-
-if getent group www-data >/dev/null; then
-    usermod -aG www-data caddy
-fi
-
-chown -R caddy:caddy /var/lib/caddy
-
-VERSION_SLUG=${CADDY_VERSION//v/}
-wget -qO /tmp/caddy.tar.gz https://github.com/caddyserver/caddy/releases/download/"${CADDY_VERSION}"/caddy_"${VERSION_SLUG}"_linux_amd64.tar.gz
-tar -xzf /tmp/caddy.tar.gz -C /usr/bin caddy
-chmod +x /usr/bin/caddy
-setcap cap_net_bind_service=+ep /usr/bin/caddy
-
-mkdir /usr/share/caddy/
-chmod 755 /usr/share/caddy/
+echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" | tee -a /etc/apt/sources.list.d/caddy-fury.list
+apt update
+apt install caddy
 
 mkdir -p /var/www/html
 chown -R caddy:caddy /var/www
-
-mkdir /etc/caddy
